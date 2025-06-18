ba95940d15c43bab45fa1c354390b96f487e4c58
mfthfarid <mfthfarid1826@gmail.com>
2025-06-18 14:55:26 +0700

    Awal

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
@@ -34,7 +34,7 @@ class _MyHomePageState extends State {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: (DataPelanggan()),
-    );
+        // body: (DataPelanggan()),
+        );
   }
 }
